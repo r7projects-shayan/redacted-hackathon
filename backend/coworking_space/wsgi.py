6c62003a35cc0d4074c859c363cd15c11d97774b
Ayobami6 <ayobamidele006@gmail.com>
2024-10-01 09:39:29 +0100

    Initial project set up

--- backend/coworking_space/wsgi.py
+++ backend/coworking_space/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for startovr project.
+WSGI config for coworking_space project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'startovr.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coworking_space.settings')
 
 application = get_wsgi_application()
