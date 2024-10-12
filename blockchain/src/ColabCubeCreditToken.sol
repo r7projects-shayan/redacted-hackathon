c248a1890bc4ba22393670e355b63a8827589378
codypharm <williamikeji@gmail.com>
2024-10-12 12:31:44 +0100

    fix: from manage role to role based management

--- blockchain/src/ColabCubeCreditToken.sol
+++ blockchain/src/ColabCubeCreditToken.sol
@@ -1,20 +1,55 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/manager/AccessManaged.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract ColabCubeCreditToken is ERC20, ERC20Burnable, AccessManaged, ERC20Permit {
-    constructor(address initialAuthority)
-        ERC20("ColabCubeCreditToken", "CCT")
-        AccessManaged(initialAuthority)
-        ERC20Permit("ColabCubeCreditToken")
-    {}
+contract ColabCubeCreditToken is ERC20, ERC20Burnable, ERC20Pausable, AccessControl, ERC20Permit {
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
-    function mint(address to, uint256 amount) public restricted {
+    constructor(address defaultAdmin, address pauser, address minter, address burner)
+        ERC20("ColabCubeCreditToken", "CCT")
+        ERC20Permit("ColabCubeCreditToken")
+    {
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(PAUSER_ROLE, pauser);
+        _grantRole(MINTER_ROLE, minter);
+        _grantRole(BURNER_ROLE, burner);
+    }
+
+    // Pausing functionality restricted to PAUSER_ROLE
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    // Unpausing functionality restricted to PAUSER_ROLE
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    // Minting functionality restricted to MINTER_ROLE
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
+    }
+
+    // Burning functionality restricted to BURNER_ROLE
+    function burn(uint256 amount) public override onlyRole(BURNER_ROLE) {
+        super.burn(amount);
+    }
+
+    // Burning from a specific account restricted to BURNER_ROLE
+    function burnFrom(address account, uint256 amount) public override onlyRole(BURNER_ROLE) {
+        super.burnFrom(account, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Pausable) {
+        super._update(from, to, value);
     }
 }
