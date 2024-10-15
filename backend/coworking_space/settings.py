a1e26920d5455261a528cf060d8c4e32576a41d0
MachaelAnderson-AI <Machael.Anthoney.Anderson@gmail.com>
2024-10-15 08:18:10 +0800

    bubble star

--- backend/coworking_space/settings.py
+++ backend/coworking_space/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
