fa872ec66d576087db570ac0b3e0fb8231851dea
MichaelAnderson-AI <michael.anthoney.anderson@gmail.com>
2024-10-24 03:11:47 +0800

    add: larkapi initial build but too basic, so needed to modify

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegisterAPIView, LoginAPIView
+from .views import RegisterAPIView, LoginAPIView, LarkAPIView
 
 urlpatterns = [
     path("auth/register", RegisterAPIView.as_view(), name="register"),
     path("auth/login", LoginAPIView.as_view(), name="login"),
+    path('send_lark_message/', LarkAPIView.send_lark_message, name='send_lark_message'),
 ]
 
