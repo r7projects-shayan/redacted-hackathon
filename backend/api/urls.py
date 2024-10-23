f83a3a03506b3648555bfbe96b661f530ef34068
MichaelAnderson-AI <michael.anthoney.anderson@gmail.com>
2024-10-24 03:15:16 +0800

    fix: Larkapi seperated

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import RegisterAPIView, LoginAPIView, LarkAPIView
+from .views import RegisterAPIView, LoginAPIView
+from .views import LarkAPIView
 
 urlpatterns = [
     path("auth/register", RegisterAPIView.as_view(), name="register"),
+
     path("auth/login", LoginAPIView.as_view(), name="login"),
+
     path('send_lark_message/', LarkAPIView.send_lark_message, name='send_lark_message'),
 ]
 
